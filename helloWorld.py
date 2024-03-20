9b9fbb4117462b8984d13fe6f954f0bb399c9543
geataharon <geataharon@gmail.com>
2024-03-20 15:43:30 +0000

    Implement user input-response feature

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,11 @@
 name = input('please enter your name: ')
 print('Hi there', name)
 
+mood = int(input('On a scale of 1-5 (1 being the worst and 5 the best): How are you today?'))
+
+if mood >3:
+    print("That's the spirit! enjoy the rest of your day")
+elif mood == 3:
+    print("Balance is key!, wishing you a lovely day")
+else:
+    print("I'm sorry to hear that, hope your day gets better")
